cf201d40dec94b3fb00e07b2bb4598c0d78369aa
Kazauwa <i.heart.d.dead@gmail.com>
2017-01-06 00:24:43 +0300

    add first revision of script

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Allows to bulk check health of given websites
 
-# Project Goals
+## Usage
+```
+pip install -r requirements.txt
+python check_sites_health.py -i --input_file [-d --prepaid_delta]
+```
+Requires a path to the list of urls in a .txt file as an input. One full (http://www.example.com) per line. Outputs result on health checks. 
+
+## Options
+
+`-i --input_file` - path to the .txt file with a list of urls
+
+`-d --prepaid_delta' - minimal number of days to be prepaid in advance for domain. Default is 30 days.
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,57 @@
+import requests
+from argparse import ArgumentParser
+from whois import whois
+from datetime import timedelta, datetime
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as reader:
+        urls = reader.read().splitlines()
+        return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get(url)
+    return response.status_code == 200
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def has_domain_been_prepaid(expiration_date, delta_days):
+    now = datetime.now()
+    return (expiration_date - now) > timedelta(days=delta_days)
+
+
+def get_expiration_date(domain):
+    expiration_date = whois(url).expiration_date
+    if isinstance(expiration_date, list):
+        expiration_date = expiration_date[0]
+    return expiration_date
+
+
+def check_site_health(url, **kwargs):
+    expiration_date = get_expiration_date(url)
+    result = {
+        'url': url,
+        'is_avaliable': is_server_respond_with_200(url),
+        'is_prepaid': has_domain_been_prepaid(expiration_date, kwargs['prepaid_delta'])
+    }
+    return result
+
+
+def output_result_to_console(results):
+    print('\nDomain: %s' % results['url'])
+    print('Responds with 200: %s' % results['is_avaliable'])
+    print('Prepaid in advance: %s' % results['is_prepaid'])
+
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser(description='Check avaliability of give urls')
+    parser.add_argument('-i', '--input_file', type=str, nargs='?', required=True, dest='input_file',
+                        help='Path to .txt file with urls to check')
+    parser.add_argument('-d', '--prepaid_delta', type=int, nargs='?', default=30,
+                        help='Minimal number of days to be prepaid in advance for domain. Default is 30 days.')
+    options = parser.parse_args()
+
+    urls = load_urls4check(options.input_file)
+    for url in urls:
+        result = check_site_health(url, prepaid_delta=options.prepaid_delta)
+        output_result_to_console(result)
